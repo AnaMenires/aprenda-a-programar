3a160cae64efa668ad22b350f2a35cade9e097c0
Ana Menires <anamenires@gmail.com>
2022-08-23 23:15:26 -0300

    valores adicionados ao js.1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+nome="anaMenires"
+email="anamenires@gmail.com"
+profissao= "publicitaria"
+idade= "27"
+lugarDosSonhos= "andorra"
+
+
+console.log("Ana Menires")
+console.log("anamenires@gmail.com")
+console.log("publicitaria")
+console.log("27")
+console.log("Andorra")
