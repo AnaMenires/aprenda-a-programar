d2afc1f34d80de943f8e1416fdadb4029ff06cc2
Ana Menires <anamenires@gmail.com>
2022-08-23 23:24:45 -0300

    lógica de variaveis aplicada ao desafio

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,12 +1,10 @@
-nome="anaMenires"
-email="anamenires@gmail.com"
-profissao= "publicitaria"
-idade= "27"
-lugarDosSonhos= "andorra"
+let nome="Ana Menires"
+let email="anamenires@gmail.com"
+let profissao= "publicitaria"
+let idade= "27"
+let lugarDosSonhos= "andorra"
+
+let mensagem = "Olá " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos escrevendo esse email, pois acabamos de confirmar a compra de suas passagens áereas para" + lugarDosSonhos + ".Confirma pra gente alguns dados? A sua idade é" + idade + "você é" + profissao + "e seu email é" + email + "? Ficamos no aguardo. Muito obrigado e boa viagem"
 
 
-console.log("Ana Menires")
-console.log("anamenires@gmail.com")
-console.log("publicitaria")
-console.log("27")
-console.log("Andorra")
+console.log(mensagem)
